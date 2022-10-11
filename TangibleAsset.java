b9ef2fe02bb2c8f298bc47f5afb7189437d9880c
yui-nakazima <115331632+yui-nakazima@users.noreply.github.com>
2022-10-11 10:56:18 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,14 @@
+public abstract class TangibleAsset {
+  private String name;
+  private int price;
+  private String color;
+  public TangibleAsset(String name,int age, String color) {
+    this.name = name;
+    this.price = price;
+    this. color = color;
+  }
+  
+  public Stering getName() { return this.name; }
+  public int getPrice() {return this.price; }
+  public String getColor() { return this.Color; }
+}
